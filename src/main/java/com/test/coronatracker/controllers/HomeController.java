e2c1afa945b87d848235ceef16f06ca57ee0103c
prabath1998 <prabhathudayanga1998@gmail.com>
2022-06-17 21:33:40 +0530

    Add features

--- src/main/java/com/test/coronatracker/controllers/HomeController.java
+++ src/main/java/com/test/coronatracker/controllers/HomeController.java
@@ -1,14 +1,28 @@
 package com.test.coronatracker.controllers;
 
+import com.test.coronatracker.models.LocationStats;
+import com.test.coronatracker.services.coronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
+    @Autowired
+    coronaVirusDataService coronavirusDataService;
     @GetMapping("/")
     public String home(Model model){
-        model.addAttribute("testName","Test");
+        List<LocationStats> allStats = coronavirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats",allStats);
+        model.addAttribute("totalReportedCases",totalReportedCases);
+        model.addAttribute("totalNewCases",totalNewCases);
+
+
         return "home";
     }
 }
--- src/main/java/com/test/coronatracker/services/coronaVirusDataService.java
+++ src/main/java/com/test/coronatracker/services/coronaVirusDataService.java
@@ -21,6 +21,10 @@ public class coronaVirusDataService {
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
     private List<LocationStats> allStats = new ArrayList<>();
 
+    public List<LocationStats> getAllStats() {
+        return allStats;
+    }
+
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
@@ -38,8 +42,11 @@ public class coronaVirusDataService {
             LocationStats locationStat = new LocationStats();
             locationStat.setState(record.get("Province/State"));
             locationStat.setCountry(record.get("Country/Region"));
-            locationStat.setLatestTotalCases(Integer.parseInt(record.get(record.size()-1)));
-            System.out.println(locationStat);
+            int latestCases = Integer.parseInt(record.get(record.size()-1));
+            int prevDayCases = Integer.parseInt(record.get(record.size()-2));
+            locationStat.setLatestTotalCases(latestCases);
+            locationStat.setDiffFromPrevDay(latestCases - prevDayCases);
+//            System.out.println(locationStat);
             newStats.add(locationStat);
         }
         this.allStats = newStats;
