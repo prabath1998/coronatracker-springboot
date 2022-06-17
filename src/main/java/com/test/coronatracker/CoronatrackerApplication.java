5af5424aba9357fb553ab1d305fe46300459e8f4
prabath1998 <prabhathudayanga1998@gmail.com>
2022-06-17 08:15:42 +0530

    Initial commit

---
+++ src/main/java/com/test/coronatracker/CoronatrackerApplication.java
@@ -0,0 +1,13 @@
+package com.test.coronatracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronatrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronatrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/coronatracker/CoronatrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.coronatracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronatrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
